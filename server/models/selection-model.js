350e535821621dba77522867139a558333f707ed
bvenceslas <bvenceslas@gmail.com>
2021-11-27 10:58:53 +0200

    selection model

--- server/models/selection-model.js
+++ server/models/selection-model.js
@@ -1,34 +1,38 @@
 const { model, Schema } = require('mongoose');
 
 const OffreSchema = new Schema({
-    nom_organisation: {
-        type: String,
-        required: true
+    offre: {
+        type: Schema.Types.ObjectId,
+        ref: 'Offre'
     },
-    poste: {
-        type: String,
-        required: true
-    },
-    lieu_affectation: {
-        type: String,
-        required: true
-    },
-    date_publication: {
-        type: Date,
-        default: Date.now()
+    candidat: {
+        type: Schema.Types.ObjectId,
+        ref: 'Candidat'
     },
     date_limite: {
         type: Date,
         required: true
     },
-    details_offre: {
-        type: String,
-        required: true
-    },
     email: {
         type: String,
         required: true
-    }  
+    },
+    heure_fin: {
+        type: String,
+        required: true
+    },
+    interview_audio: {
+        type: String,
+        required: true
+    },
+    interview_video: {
+        type: String,
+        required: true
+    },
+    test: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = model('Offre', OffreSchema);
